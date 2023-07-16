344dcdcb4a1b4b7b8ce1f551e94e612e74e2e27e
palindungan <44224923+palindungan@users.noreply.github.com>
2023-07-16 22:44:26 +0700

    Revert "Update FaceMeshBasic.py"
    This reverts commit 9fcef23d36a2e0c4c5baac39a56b801065d52afa.

--- Src/FaceMeshBasic.py
+++ Src/FaceMeshBasic.py
@@ -64,32 +64,3 @@ while True:
     # action for end proses
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
-
-
-def draw_face_shape(image, face_landmarks):
-    # Gambar poligon untuk menggambarkan bentuk wajah
-    # Bentuk wajah yang diambil berdasarkan landmark Mediapipe
-    face_contour = [
-        face_landmarks[0], face_landmarks[1], face_landmarks[2], face_landmarks[3],
-        face_landmarks[4], face_landmarks[5], face_landmarks[6], face_landmarks[7],
-        face_landmarks[8], face_landmarks[9], face_landmarks[10], face_landmarks[11],
-        face_landmarks[12], face_landmarks[13], face_landmarks[14], face_landmarks[15],
-        face_landmarks[16], face_landmarks[17], face_landmarks[18], face_landmarks[19],
-        face_landmarks[20], face_landmarks[21], face_landmarks[22], face_landmarks[23],
-        face_landmarks[24], face_landmarks[25], face_landmarks[26], face_landmarks[27],
-        face_landmarks[28], face_landmarks[29], face_landmarks[30], face_landmarks[31],
-        face_landmarks[32], face_landmarks[33], face_landmarks[34], face_landmarks[35],
-        face_landmarks[36], face_landmarks[39], face_landmarks[40], face_landmarks[41],
-        face_landmarks[42], face_landmarks[43], face_landmarks[44], face_landmarks[45],
-        face_landmarks[46], face_landmarks[47], face_landmarks[48], face_landmarks[49],
-        face_landmarks[50], face_landmarks[51], face_landmarks[52], face_landmarks[53],
-        face_landmarks[54], face_landmarks[55], face_landmarks[56], face_landmarks[57],
-        face_landmarks[58], face_landmarks[59], face_landmarks[60], face_landmarks[61],
-        face_landmarks[62], face_landmarks[63], face_landmarks[64], face_landmarks[65],
-        face_landmarks[66], face_landmarks[67]
-    ]
-
-    # Gambar bentuk wajah dengan poligon
-    cv2.polylines(image, [face_contour], isClosed=True, color=(0, 255, 0), thickness=2)
-
-    return image
