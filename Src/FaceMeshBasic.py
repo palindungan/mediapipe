f42a42a5113ac3f731cb5555d75ce2f576500b8f
palindungan <44224923+palindungan@users.noreply.github.com>
2023-07-16 14:19:18 +0700

    Update FaceMeshBasic.py

--- Src/FaceMeshBasic.py
+++ Src/FaceMeshBasic.py
@@ -10,20 +10,7 @@ imageProcessing = ImageProcessingModule.ImageProcessing()
 # End of Declare Object Class
 
 # Start of Setting
-##################
-wCam, hCam = 480, 360  # width and height image
-noCam = 1  # default Cam
 globalColor = (255, 0, 0)  # default color
-detectionCon = 0.70  # set Confident in AI Mediapipe
-
-cameraBrightness = 190  # Set Brightness
-moduleVal = 5  # SAVE EVERY 1 FRAME TO AVOID REPETITION
-minBlur = 500  # SMALLER VALUE MEANS MORE BLURRINESS PRESENT
-grayImage = False  # IMAGE SAVED COLORED OR GRAY
-saveData = False  # SAVE DATA FLAG
-imgWidth = 180  # Resize width Image
-imgHeight = 120  # Resize height Image
-##################
 # End of Setting
 
 cap = cv2.VideoCapture(basicTools.getBaseUrl() + "/Resource/Videos/2.mp4")
