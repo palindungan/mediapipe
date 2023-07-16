9b9fc13969ccf4ea764c281131e94fbca0afdb3d
palindungan <44224923+palindungan@users.noreply.github.com>
2023-07-17 00:09:55 +0700

    Update FaceMesh.py

--- Src/FaceMesh.py
+++ Src/FaceMesh.py
@@ -1,66 +1,50 @@
 import cv2
 import mediapipe as mp
-import time
 from Util import BasicToolModule
-from Util import ImageProcessingModule
 
-# Start of Declare Object Class
-basicTools = BasicToolModule.BasicTools()
-imageProcessing = ImageProcessingModule.ImageProcessing()
-# End of Declare Object Class
+def draw_outer_face_edges(image_path):
+    # Inisialisasi MediaPipe FaceMesh
+    mp_face_mesh = mp.solutions.face_mesh.FaceMesh(
+        static_image_mode=True,
+        max_num_faces=1,  # Hanya mencari satu wajah pada gambar
+        min_detection_confidence=0.5
+    )
 
-# Start of Setting
-wCam, hCam = 480, 360  # width and height image
-noCam = 1  # default Cam
-cameraBrightness = 190  # Set Brightness
+    # Baca gambar dari path
+    image = cv2.imread(image_path)
 
-globalColor = (0, 255, 0)  # default color
-# End of Setting
+    # Konversi gambar menjadi RGB (MediaPipe menggunakan gambar dalam format RGB)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-cap = cv2.VideoCapture(basicTools.getBaseUrl() + "/Resource/Videos/3.mp4")
-# cap = cv2.VideoCapture(noCam)
-# cap.set(3, wCam)
-# cap.set(4, hCam)
-# cap.set(10, cameraBrightness)
+    # Deteksi wajah pada gambar
+    results = mp_face_mesh.process(image_rgb)
 
-mpDraw = mp.solutions.drawing_utils
-mpFaceMesh = mp.solutions.face_mesh
-faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
-drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=1, color=globalColor)
-
-while True:
-    success, img = cap.read()
-
-    imgOri = img.copy()
-    imgContours = basicTools.CreateBlankImage(img)
-
-    # detect FaceMesh
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = faceMesh.process(imgRGB)
+    # Mendapatkan koordinat tepi wajah terluar
+    face_edges = []
     if results.multi_face_landmarks:
-        for faceId, faceLms in enumerate(results.multi_face_landmarks):
-            # Draw the face mesh default
-            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawSpec, drawSpec)
-            mpDraw.draw_landmarks(imgContours, faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawSpec, drawSpec)
-            # Draw the face mesh manual
-            # for idx, landmark in enumerate(faceLms.landmark):
-            #     x, y = int(landmark.x * img.shape[1]), int(landmark.y * img.shape[0])
-            #     cv2.circle(img, (x, y), radius=1, color=(0, 255, 0), thickness=-1)
-            #     cv2.circle(imgContours, (x, y), radius=1, color=(0, 255, 0), thickness=-1)
+        for face_landmarks in results.multi_face_landmarks:
+            outer_edges = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323,
+                           361, 288, 397, 365, 379, 378, 400, 377, 152, 148,
+                           176, 149, 150, 136, 172, 58, 132, 93, 234, 127,
+                           162, 21, 54, 103, 67, 109, 10]
+            for idx in outer_edges:
+                landmark = face_landmarks.landmark[idx]
+                x, y = int(landmark.x * image.shape[1]), int(landmark.y * image.shape[0])
+                face_edges.append((x, y))
 
-            # print landmark
-            for idx, lm in enumerate(faceLms.landmark):
-                ih, iw, ic = img.shape
-                x, y = int(lm.x * iw), int(lm.y * ih)
-                print("face: " + str(faceId) + ", id:" + str(idx), ", x:" + str(x), ", y:" + str(y))
+                # Gambar titik-titik tepi wajah pada gambar
+                cv2.circle(image, (x, y), 1, (0, 255, 0), -1)
 
-    fps = basicTools.countFps(time=time.time())
-    cv2.putText(imgOri, f'FPS {int(fps)}', (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, globalColor, 3)
+    return image
 
-    # show result in stacked images
-    stackedImages = imageProcessing.stackImages(1, ([imgOri, imgContours], [img, basicTools.CreateBlankImage(img)]))
-    cv2.imshow("Stacked Image", stackedImages)
+if __name__ == "__main__":
+    basicTools = BasicToolModule.BasicTools()
 
-    # action for end proses
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
+    image_path = basicTools.getBaseUrl() + '/Resource/Images/1.png'
+
+    result_image = draw_outer_face_edges(image_path)
+
+    # Tampilkan gambar dengan titik-titik tepi wajah terluar
+    cv2.imshow("Outer Face Edges", result_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
