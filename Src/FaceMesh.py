7bdb9878648e5649fcc06f8c7b04d227f4b74cd4
palindungan <44224923+palindungan@users.noreply.github.com>
2023-07-16 20:34:23 +0700

    Update FaceMesh.py

--- Src/FaceMesh.py
+++ Src/FaceMesh.py
@@ -4,63 +4,32 @@ import time
 from Util import BasicToolModule
 from Util import ImageProcessingModule
 
-# Start of Declare Object Class
-basicTools = BasicToolModule.BasicTools()
-imageProcessing = ImageProcessingModule.ImageProcessing()
-# End of Declare Object Class
+def main():
+    # Start of Declare Object Class
+    basicTools = BasicToolModule.BasicTools()
+    imageProcessing = ImageProcessingModule.ImageProcessing()
+    # End of Declare Object Class
 
-# Start of Setting
-wCam, hCam = 480, 360  # width and height image
-noCam = 1  # default Cam
-cameraBrightness = 190  # Set Brightness
+    globalColor = (0, 255, 0)  # default color
 
-globalColor = (0, 255, 0)  # default color
-# End of Setting
+    cap = cv2.VideoCapture(basicTools.getBaseUrl() + "/Resource/Videos/3.mp4")
 
-cap = cv2.VideoCapture(basicTools.getBaseUrl() + "/Resource/Videos/3.mp4")
-# cap = cv2.VideoCapture(noCam)
-# cap.set(3, wCam)
-# cap.set(4, hCam)
-# cap.set(10, cameraBrightness)
+    while True:
+        success, img = cap.read()
 
-mpDraw = mp.solutions.drawing_utils
-mpFaceMesh = mp.solutions.face_mesh
-faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
-drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=1, color=globalColor)
+        imgOri = img.copy()
+        imgContours = basicTools.CreateBlankImage(img)
 
-while True:
-    success, img = cap.read()
+        fps = basicTools.countFps(time=time.time())
+        cv2.putText(imgOri, f'FPS {int(fps)}', (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, globalColor, 3)
 
-    imgOri = img.copy()
-    imgContours = basicTools.CreateBlankImage(img)
+        # show result in stacked images
+        stackedImages = imageProcessing.stackImages(1, ([imgOri, imgContours], [img, basicTools.CreateBlankImage(img)]))
+        cv2.imshow("Stacked Image", stackedImages)
 
-    # detect FaceMesh
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = faceMesh.process(imgRGB)
-    if results.multi_face_landmarks:
-        for faceId, faceLms in enumerate(results.multi_face_landmarks):
-            # Draw the face mesh default
-            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawSpec, drawSpec)
-            mpDraw.draw_landmarks(imgContours, faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawSpec, drawSpec)
-            # Draw the face mesh manual
-            # for idx, landmark in enumerate(faceLms.landmark):
-            #     x, y = int(landmark.x * img.shape[1]), int(landmark.y * img.shape[0])
-            #     cv2.circle(img, (x, y), radius=1, color=(0, 255, 0), thickness=-1)
-            #     cv2.circle(imgContours, (x, y), radius=1, color=(0, 255, 0), thickness=-1)
+        # action for end proses
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
 
-            # print landmark
-            for idx, lm in enumerate(faceLms.landmark):
-                ih, iw, ic = img.shape
-                x, y = int(lm.x * iw), int(lm.y * ih)
-                print("face: " + str(faceId) + ", id:" + str(idx), ", x:" + str(x), ", y:" + str(y))
-
-    fps = basicTools.countFps(time=time.time())
-    cv2.putText(imgOri, f'FPS {int(fps)}', (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, globalColor, 3)
-
-    # show result in stacked images
-    stackedImages = imageProcessing.stackImages(1, ([imgOri, imgContours], [img, basicTools.CreateBlankImage(img)]))
-    cv2.imshow("Stacked Image", stackedImages)
-
-    # action for end proses
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
+if __name__ == '__main__':
+    main()
