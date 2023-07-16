f71d4dcad1f888b46530cea5c26c75f25e31e7ba
palindungan <44224923+palindungan@users.noreply.github.com>
2023-07-16 23:48:38 +0700

    Update FaceMesh.py

--- Src/FaceMesh.py
+++ Src/FaceMesh.py
@@ -1,40 +1,8 @@
 import cv2
 import mediapipe as mp
 import time
-import pandas as pd
 from Util import BasicToolModule
 from Util import ImageProcessingModule
-
-
-def getFaceOval():
-    face_oval = mpFaceMesh.FACEMESH_FACE_OVAL
-    df = pd.DataFrame(list(face_oval), columns=["p1", "p2"])
-    print(df)
-
-    routes_idx = []
-
-    p1 = df.iloc[0]["p1"]
-    p2 = df.iloc[0]["p2"]
-
-    for i in range(0, df.shape[0]):
-        # print(p1, p2)
-
-        obj = df[df["p1"] == p2]
-        p1 = obj["p1"].values[0]
-        p2 = obj["p2"].values[0]
-
-        route_idx = []
-        route_idx.append(p1)
-        route_idx.append(p2)
-        routes_idx.append(route_idx)
-
-    # -------------------------------
-
-    for route_idx in routes_idx:
-        print(f"Draw a line between {route_idx[0]}th landmark point to {route_idx[1]}th landmark point")
-
-    return routes_idx
-
 
 # Start of Declare Object Class
 basicTools = BasicToolModule.BasicTools()
@@ -58,10 +26,7 @@ cap = cv2.VideoCapture(basicTools.getBaseUrl() + "/Resource/Videos/3.mp4")
 mpDraw = mp.solutions.drawing_utils
 mpFaceMesh = mp.solutions.face_mesh
 faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
-
 drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=1, color=globalColor)
-
-routes_id = getFaceOval()
 
 while True:
     success, img = cap.read()
@@ -77,11 +42,17 @@ while True:
             # Draw the face mesh default
             mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawSpec, drawSpec)
             mpDraw.draw_landmarks(imgContours, faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawSpec, drawSpec)
+            # Draw the face mesh manual
+            # for idx, landmark in enumerate(faceLms.landmark):
+            #     x, y = int(landmark.x * img.shape[1]), int(landmark.y * img.shape[0])
+            #     cv2.circle(img, (x, y), radius=1, color=(0, 255, 0), thickness=-1)
+            #     cv2.circle(imgContours, (x, y), radius=1, color=(0, 255, 0), thickness=-1)
 
             # print landmark
             for idx, lm in enumerate(faceLms.landmark):
                 ih, iw, ic = img.shape
                 x, y = int(lm.x * iw), int(lm.y * ih)
+                print("face: " + str(faceId) + ", id:" + str(idx), ", x:" + str(x), ", y:" + str(y))
 
     fps = basicTools.countFps(time=time.time())
     cv2.putText(imgOri, f'FPS {int(fps)}', (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, globalColor, 3)
