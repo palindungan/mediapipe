db8ff04e037e7a2d6a444ed487aaad5a8579e722
palindungan <44224923+palindungan@users.noreply.github.com>
2023-07-17 00:30:07 +0700

    Update FaceMeshCrop.py

--- Src/FaceMeshCrop.py
+++ Src/FaceMeshCrop.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import numpy as np
 from Util import BasicToolModule
 from Util import ImageProcessingModule
 
@@ -33,8 +34,11 @@ while True:
 
     imgOri = img.copy()
     imgContours = basicTools.CreateBlankImage(img)
+    imgROI = img.copy()
 
     # detect FaceMesh
+    face_edges_list = []
+
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = faceMesh.process(imgRGB)
     if results.multi_face_landmarks:
@@ -49,11 +53,36 @@ while True:
                 x, y = int(lm.x * iw), int(lm.y * ih)
                 print("face: " + str(faceId) + ", id:" + str(idx), ", x:" + str(x), ", y:" + str(y))
 
+            face_edges = []
+            outer_edges = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323,
+                           361, 288, 397, 365, 379, 378, 400, 377, 152, 148,
+                           176, 149, 150, 136, 172, 58, 132, 93, 234, 127,
+                           162, 21, 54, 103, 67, 109, 10]
+            for idx in outer_edges:
+                landmark = faceLms.landmark[idx]
+                x, y = int(landmark.x * img.shape[1]), int(landmark.y * img.shape[0])
+                face_edges.append((x, y))
+            face_edges_list.append(face_edges)
+
+    # Gambar poligon wajah terluar pada semua wajah
+    mask = np.zeros_like(imgROI)
+    for face_edges in face_edges_list:
+        points = np.array(face_edges, np.int32)
+        cv2.fillPoly(mask, [points], (255, 255, 255))
+
+    # Gambar hitam di luar poligon wajah terluar
+    imgROI[~mask.any(axis=2)] = 0
+
+    # Gambar titik-titik tepi wajah terluar pada semua wajah
+    for face_edges in face_edges_list:
+        for x, y in face_edges:
+            cv2.circle(imgROI, (x, y), 1, (0, 255, 0), -1)
+
     fps = basicTools.countFps(time=time.time())
     cv2.putText(imgOri, f'FPS {int(fps)}', (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, globalColor, 3)
 
     # show result in stacked images
-    stackedImages = imageProcessing.stackImages(1, ([imgOri, imgContours], [img, basicTools.CreateBlankImage(img)]))
+    stackedImages = imageProcessing.stackImages(1, ([imgOri, imgContours], [img, imgROI]))
     cv2.imshow("Stacked Image", stackedImages)
 
     # action for end proses
