1de4f2ac9eb4cb46ce803304f25c6f2d11528fd6
palindungan <44224923+palindungan@users.noreply.github.com>
2023-07-30 18:50:14 +0700

    Update MediapipeFaceMeshModule.py

--- Util/MediapipeFaceMeshModule.py
+++ Util/MediapipeFaceMeshModule.py
@@ -7,27 +7,27 @@ from Util import BasicToolModule
 
 class MediapipeFaceMesh:
     def __init__(self):
-        self.globalColor = (0, 255, 0)
-        self.imgHeight, self.imgWidth, self.imgChannel = 0, 0, 0
+        self.global_color = (0, 255, 0)
+        self.img_height, self.img_width, self.img_channel = 0, 0, 0
 
-        self.basicTools = BasicToolModule.BasicTool()
+        self.basicTool = BasicToolModule.BasicTool()
 
         # Mediapipe Class
         self.mpFaceMesh = mp.solutions.face_mesh
         self.faceMesh = self.mpFaceMesh.FaceMesh(max_num_faces=2)
         self.mpDrawingSpec = mp.solutions.drawing_utils
-        self.drawingSpec = self.mpDrawingSpec.DrawingSpec(thickness=1, circle_radius=1, color=self.globalColor)
+        self.drawingSpec = self.mpDrawingSpec.DrawingSpec(thickness=1, circle_radius=1, color=self.global_color)
 
     def processing(self, img):
-        self.imgHeight, self.imgWidth, self.imgChannel = img.shape
+        self.img_height, self.img_width, self.img_channel = img.shape
 
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.faceMesh.process(imgRGB)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.faceMesh.process(img_rgb)
         return results.multi_face_landmarks
 
-    def drawing_img(self, img, multiFaceLandmarks):
-        if multiFaceLandmarks:
-            for faceId, faceLandmarks in enumerate(multiFaceLandmarks):
+    def drawing_img(self, img, multi_face_landmarks):
+        if multi_face_landmarks:
+            for faceId, faceLandmarks in enumerate(multi_face_landmarks):
                 # Draw the face mesh default
                 self.mpDrawingSpec.draw_landmarks(img,
                                                   faceLandmarks,
@@ -42,30 +42,30 @@ class MediapipeFaceMesh:
 
         return img
 
-    def drawing_roi(self, img, multiFaceLandmarks):
-        edgesId = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323,
-                   361, 288, 397, 365, 379, 378, 400, 377, 152, 148,
-                   176, 149, 150, 136, 172, 58, 132, 93, 234, 127,
-                   162, 21, 54, 103, 67, 109, 10]
-        edgesArray = []
+    def drawing_roi(self, img, multi_face_landmarks):
+        edges_id = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323,
+                    361, 288, 397, 365, 379, 378, 400, 377, 152, 148,
+                    176, 149, 150, 136, 172, 58, 132, 93, 234, 127,
+                    162, 21, 54, 103, 67, 109, 10]
+        edges_array = []
 
-        if multiFaceLandmarks:
-            for faceId, faceLandmarks in enumerate(multiFaceLandmarks):
+        if multi_face_landmarks:
+            for faceId, faceLandmarks in enumerate(multi_face_landmarks):
                 array = []
-                for idx in edgesId:
+                for idx in edges_id:
                     landmark = faceLandmarks.landmark[idx]
-                    x, y = int(landmark.x * self.imgWidth), int(landmark.y * self.imgHeight)
+                    x, y = int(landmark.x * self.img_width), int(landmark.y * self.img_height)
                     array.append((x, y))
-                edgesArray.append(array)
+                edges_array.append(array)
 
         mask = np.zeros_like(img)
-        for edges in edgesArray:
+        for edges in edges_array:
             points = np.array(edges, np.int32)
             cv2.fillPoly(mask, [points], (255, 255, 255))
 
         img[~mask.any(axis=2)] = 0
 
-        for edges in edgesArray:
+        for edges in edges_array:
             for x, y in edges:
                 cv2.circle(img, (x, y), 1, (0, 255, 0), -1)
 
