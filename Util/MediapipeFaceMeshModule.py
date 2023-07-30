6c2a17db815a8bbd4dea9f909fc8b2e975f304e5
palindungan <44224923+palindungan@users.noreply.github.com>
2023-07-30 17:10:05 +0700

    Update MediapipeFaceMeshModule.py

--- Util/MediapipeFaceMeshModule.py
+++ Util/MediapipeFaceMeshModule.py
@@ -8,10 +8,10 @@ from Util import BasicToolModule
 class MediapipeFaceMesh:
     def __init__(self):
         self.globalColor = (0, 255, 0)
-        self.faceEdgesOuter = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323,
-                               361, 288, 397, 365, 379, 378, 400, 377, 152, 148,
-                               176, 149, 150, 136, 172, 58, 132, 93, 234, 127,
-                               162, 21, 54, 103, 67, 109, 10]
+        self.faceEdgesOuterId = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323,
+                                 361, 288, 397, 365, 379, 378, 400, 377, 152, 148,
+                                 176, 149, 150, 136, 172, 58, 132, 93, 234, 127,
+                                 162, 21, 54, 103, 67, 109, 10]
 
         self.basicTools = BasicToolModule.BasicTool()
 
@@ -52,7 +52,7 @@ class MediapipeFaceMesh:
 
                 face_edges = []
 
-                for idx in self.faceEdgesOuter:
+                for idx in self.faceEdgesOuterId:
                     landmark = faceLandmarks.landmark[idx]
                     x, y = int(landmark.x * img.shape[1]), int(landmark.y * img.shape[0])
                     face_edges.append((x, y))
