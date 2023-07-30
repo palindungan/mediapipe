69333bb737f4f4278f888f3c19677d52eb28c2d0
palindungan <44224923+palindungan@users.noreply.github.com>
2023-07-30 15:46:41 +0700

    Update MediapipeFaceMeshModule.py

--- Util/MediapipeFaceMeshModule.py
+++ Util/MediapipeFaceMeshModule.py
@@ -3,9 +3,9 @@ import mediapipe as mp
 
 class MediapipeFaceMesh:
     def __init__(self):
-        globalColor = (0, 255, 0)  # default color
+        self.globalColor = (0, 255, 0)  # default color
 
-        mpFaceMesh = mp.solutions.face_mesh
-        faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
-        mpDrawingSpec = mp.solutions.drawing_utils
-        drawingSpec = mpDrawingSpec.DrawingSpec(thickness=1, circle_radius=1, color=globalColor)
+        self.mpFaceMesh = mp.solutions.face_mesh
+        self.faceMesh = self.mpFaceMesh.FaceMesh(max_num_faces=2)
+        self.mpDrawingSpec = mp.solutions.drawing_utils
+        self.drawingSpec = self.mpDrawingSpec.DrawingSpec(thickness=1, circle_radius=1, color=self.globalColor)
