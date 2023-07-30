2182381ffdb1b624650e02751c8d300b02d33c5f
palindungan <44224923+palindungan@users.noreply.github.com>
2023-07-30 19:26:53 +0700

    Update ImageProcessingModule.py

--- Util/ImageProcessingModule.py
+++ Util/ImageProcessingModule.py
@@ -18,7 +18,8 @@ class ImageProcessing:
                     else:
                         imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
                                                     None, scale, scale)
-                    if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+                    if len(imgArray[x][y].shape) == 2:
+                        imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
             imageBlank = np.zeros((height, width, 3), np.uint8)
             hor = [imageBlank] * rows
             # hor_con = [imageBlank] * rows
@@ -32,7 +33,8 @@ class ImageProcessing:
                 else:
                     imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale,
                                              scale)
-                if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+                if len(imgArray[x].shape) == 2:
+                    imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
             hor = np.hstack(imgArray)
             ver = hor
         return ver
