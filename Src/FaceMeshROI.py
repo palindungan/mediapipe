366b27fd94f797933eaa4ddc5961d702b1d34156
palindungan <44224923+palindungan@users.noreply.github.com>
2023-07-30 16:10:43 +0700

    MediapipeFaceMesh processing

--- Src/FaceMeshROI.py
+++ Src/FaceMeshROI.py
@@ -1,14 +1,14 @@
 import cv2
-import mediapipe as mp
 import time
-import numpy as np
 
 from Util import BasicToolModule
 from Util import ImageProcessingModule
+from Util import MediapipeFaceMeshModule
 
 # Utility Class
 basicTools = BasicToolModule.BasicTool()
 imageProcessing = ImageProcessingModule.ImageProcessing()
+mediapipeFaceMesh = MediapipeFaceMeshModule.MediapipeFaceMesh()
 
 # Camera Setting
 wCam, hCam = 480, 360  # width and height image
@@ -24,60 +24,12 @@ cap.set(3, wCam)  # width
 cap.set(4, hCam)  # height
 cap.set(10, cameraBrightness)  # brightness
 
-# Mediapipe FaceMesh
-mpFaceMesh = mp.solutions.face_mesh
-faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
-mpDrawingSpec = mp.solutions.drawing_utils
-drawingSpec = mpDrawingSpec.DrawingSpec(thickness=1, circle_radius=1, color=globalColor)
-
 while True:
     success, img = cap.read()
-
     imgOri = img.copy()
-    imgContour = basicTools.CreateBlankImage(img)
-    imgROI = img.copy()
 
-    # detect FaceMesh
-    face_edges_list = []
-
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = faceMesh.process(imgRGB)
-    if results.multi_face_landmarks:
-        for faceId, faceLms in enumerate(results.multi_face_landmarks):
-            # Draw the face mesh default
-            mpDrawingSpec.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawingSpec, drawingSpec)
-            mpDrawingSpec.draw_landmarks(imgContour, faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawingSpec, drawingSpec)
-
-            # print landmark
-            for idx, lm in enumerate(faceLms.landmark):
-                ih, iw, ic = img.shape
-                x, y = int(lm.x * iw), int(lm.y * ih)
-                print("face: " + str(faceId) + ", id:" + str(idx), ", x:" + str(x), ", y:" + str(y))
-
-            face_edges = []
-            outer_edges = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323,
-                           361, 288, 397, 365, 379, 378, 400, 377, 152, 148,
-                           176, 149, 150, 136, 172, 58, 132, 93, 234, 127,
-                           162, 21, 54, 103, 67, 109, 10]
-            for idx in outer_edges:
-                landmark = faceLms.landmark[idx]
-                x, y = int(landmark.x * img.shape[1]), int(landmark.y * img.shape[0])
-                face_edges.append((x, y))
-            face_edges_list.append(face_edges)
-
-    # Gambar poligon wajah terluar pada semua wajah
-    mask = np.zeros_like(imgROI)
-    for face_edges in face_edges_list:
-        points = np.array(face_edges, np.int32)
-        cv2.fillPoly(mask, [points], (255, 255, 255))
-
-    # Gambar hitam di luar poligon wajah terluar
-    imgROI[~mask.any(axis=2)] = 0
-
-    # Gambar titik-titik tepi wajah terluar pada semua wajah
-    for face_edges in face_edges_list:
-        for x, y in face_edges:
-            cv2.circle(imgROI, (x, y), 1, (0, 255, 0), -1)
+    # processing
+    img, imgContour, imgROI = mediapipeFaceMesh.processing(img)
 
     # show fps
     fps = basicTools.countFps(time=time.time())
--- Util/MediapipeFaceMeshModule.py
+++ Util/MediapipeFaceMeshModule.py
@@ -1,11 +1,63 @@
+import cv2
 import mediapipe as mp
+import numpy as np
+
+from Util import BasicToolModule
 
 
 class MediapipeFaceMesh:
     def __init__(self):
         self.globalColor = (0, 255, 0)  # default color
 
+        self.basicTools = BasicToolModule.BasicTool()
+
         self.mpFaceMesh = mp.solutions.face_mesh
         self.faceMesh = self.mpFaceMesh.FaceMesh(max_num_faces=2)
         self.mpDrawingSpec = mp.solutions.drawing_utils
         self.drawingSpec = self.mpDrawingSpec.DrawingSpec(thickness=1, circle_radius=1, color=self.globalColor)
+
+    def processing(self, img):
+        imgContour = self.basicTools.CreateBlankImage(img)
+        imgROI = img.copy()
+
+        face_edges_list = []
+
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.faceMesh.process(imgRGB)
+        if results.multi_face_landmarks:
+            for faceId, faceLms in enumerate(results.multi_face_landmarks):
+                # Draw the face mesh default
+                self.mpDrawingSpec.draw_landmarks(img, faceLms, self.mpFaceMesh.FACEMESH_CONTOURS,
+                                                  self.drawingSpec, self.drawingSpec)
+                self.mpDrawingSpec.draw_landmarks(imgContour, faceLms, self.mpFaceMesh.FACEMESH_CONTOURS,
+                                                  self.drawingSpec, self.drawingSpec)
+
+                # print landmark
+                for idx, lm in enumerate(faceLms.landmark):
+                    ih, iw, ic = img.shape
+                    x, y = int(lm.x * iw), int(lm.y * ih)
+                    print("face: " + str(faceId) + ", id:" + str(idx), ", x:" + str(x), ", y:" + str(y))
+
+                face_edges = []
+                outer_edges = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323,
+                               361, 288, 397, 365, 379, 378, 400, 377, 152, 148,
+                               176, 149, 150, 136, 172, 58, 132, 93, 234, 127,
+                               162, 21, 54, 103, 67, 109, 10]
+                for idx in outer_edges:
+                    landmark = faceLms.landmark[idx]
+                    x, y = int(landmark.x * img.shape[1]), int(landmark.y * img.shape[0])
+                    face_edges.append((x, y))
+                face_edges_list.append(face_edges)
+
+        mask = np.zeros_like(imgROI)
+        for face_edges in face_edges_list:
+            points = np.array(face_edges, np.int32)
+            cv2.fillPoly(mask, [points], (255, 255, 255))
+
+        imgROI[~mask.any(axis=2)] = 0
+
+        for face_edges in face_edges_list:
+            for x, y in face_edges:
+                cv2.circle(imgROI, (x, y), 1, (0, 255, 0), -1)
+
+        return img, imgContour, imgROI
