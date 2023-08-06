c0565d4bc7f2f1202d08adfbc636ad3775f85f01
palindungan <44224923+palindungan@users.noreply.github.com>
2023-08-06 15:08:19 +0700

    Update FaceMeshTestingPrediction.py

--- Src/FaceMeshTestingPrediction.py
+++ Src/FaceMeshTestingPrediction.py
@@ -1,6 +1,8 @@
 import cv2
 import time
 import pickle
+import numpy as np
+import os
 
 from keras_facenet import FaceNet
 
@@ -29,9 +31,9 @@ cap = cv2.VideoCapture(basicTool.get_base_url() + "/Resource/Videos/3.mp4")  # r
 
 MyFaceNet = FaceNet()
 
-myfile = open("data.pkl", "rb")
-database = pickle.load(myfile)
-myfile.close()
+my_file = open(basicTool.get_base_url() + '/Resource/' + 'data.pkl', "rb")
+database = pickle.load(my_file)
+my_file.close()
 
 while True:
     success, img = cap.read()
@@ -39,14 +41,21 @@ while True:
 
     # processing
     multi_face_landmarks = mediapipeFaceMesh.processing(img)
-    img = mediapipeFaceMesh.drawing_roi(img, multi_face_landmarks)
+    get_roi_images = mediapipeFaceMesh.get_roi_images(img, multi_face_landmarks)
+
+    for roi_idx, roi_image in enumerate(get_roi_images):
+        face = cv2.cvtColor(roi_image, cv2.COLOR_BGR2RGB)
+        face = cv2.resize(face, (160, 160))
+
+        face = np.expand_dims(face, axis=0)
+        signature = MyFaceNet.embeddings(face)
 
     # show fps
     fps = basicTool.count_fps(my_time=time.time())
     cv2.putText(img_ori, f'FPS {int(fps)}', (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, global_color, 3)
 
     # show images in stacked
-    stacked_images = imageProcessing.stack_images(1, ([img_ori, img]))
+    stacked_images = imageProcessing.stack_images(1, ([img_ori]))
     cv2.imshow("Stacked Image", stacked_images)
 
     # action for end proses
