d034916491a80b00acf11f0bec1d6c57c72528b1
palindungan <44224923+palindungan@users.noreply.github.com>
2023-08-13 16:08:08 +0700

    Create FaceRecognitionModule.py

---
+++ Util/FaceRecognitionModule.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import pickle
+
+from keras_facenet import FaceNet
+
+from Util import BasicToolModule
+
+
+class FaceRecognition:
+    def __init__(self):
+        self.face_net = FaceNet()
+
+        self.basicTool = BasicToolModule.BasicTool()
+
+        model_file = open(self.basicTool.get_base_url() + '/Resource/' + 'database.pkl', "rb")
+        self.model_database = pickle.load(model_file)
+        model_file.close()
+
+    def prediction(self, roi_image):
+        face = cv2.cvtColor(roi_image, cv2.COLOR_BGR2RGB)
+        face = cv2.resize(face, (160, 160))
+
+        face = np.expand_dims(face, axis=0)
+        signature = self.face_net.embeddings(face)
+
+        min_dist = 50
+        identity = 'unknown'
+        for key, value in self.model_database.items():
+            dist = np.linalg.norm(value - signature)
+            if dist < min_dist:
+                min_dist = dist
+                identity = key
+
+        return identity
