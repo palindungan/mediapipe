e8858980674c86c578a2a81487f3ad1a45bcb9f9
palindungan <44224923+palindungan@users.noreply.github.com>
2023-08-13 10:41:16 +0700

    Update FaceMeshTestingPredictionAntiSpoofing.py

--- Src/FaceMeshTestingPredictionAntiSpoofing.py
+++ Src/FaceMeshTestingPredictionAntiSpoofing.py
@@ -5,9 +5,9 @@ import argparse
 import warnings
 import time
 
-from src.anti_spoof_predict import AntiSpoofPredict
-from src.generate_patches import CropImage
-from src.utility import parse_model_name
+from Util.AntiSpoof.anti_spoof_predict import AntiSpoofPredict
+from Util.AntiSpoof.generate_patches import CropImage
+from Util.AntiSpoof.utility import parse_model_name
 warnings.filterwarnings('ignore')
 
 
