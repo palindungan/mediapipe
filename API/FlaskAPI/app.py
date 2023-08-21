33b3ec8465606513c6447ceb8743d416966e3ba9
palindungan <44224923+palindungan@users.noreply.github.com>
2023-08-21 15:30:40 +0700

    Update app.py

--- API/FlaskAPI/app.py
+++ API/FlaskAPI/app.py
@@ -1,55 +1,42 @@
-from flask import Flask, request, jsonify, make_response
+# using flask_restful
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 
-# create an instance of flask
+# creating the flask app
 app = Flask(__name__)
 # creating an API object
 api = Api(app)
 
 
-# For GET request to http://localhost:5000/
-class GetData(Resource):
+# making a class for a particular resource
+# the get, post methods correspond to get and post requests
+# they are automatically mapped by flask_restful.
+# other methods include put, delete, etc.
+class Hello(Resource):
+
+    # corresponds to the GET request.
+    # this function is called whenever there
+    # is a GET request for this resource
     def get(self):
-        return {"data": 'success'}, 200
+        return jsonify({'message': 'hello world'})
 
-
-# For Post request to http://localhost:5000/employee
-class AddData(Resource):
+    # Corresponds to POST request
     def post(self):
-        if request.is_json:
-            # return a json response
-            return make_response(jsonify({'Id': 1, }), 201)
-        else:
-            return {'error': 'Request must be JSON'}, 400
+        data = request.get_json()  # status code
+        return jsonify({'data': data}), 201
 
 
-# For put request to http://localhost:5000/update/?
-class UpdateData(Resource):
-    def put(self, idx):
-        if request.is_json:
-            data = 1
-            if data is None:
-                return {'error': 'not found'}, 404
-            else:
-                return f'{idx} is updated', 200
-        else:
-            return {'error': 'Request must be JSON'}, 400
+# another resource to calculate the square of a number
+class Square(Resource):
+
+    def get(self, num):
+        return jsonify({'square': num ** 2})
 
 
-# For delete request to http://localhost:5000/delete/?
-class DeleteData(Resource):
-    def delete(self, idx):
-        data = 1
-        if data is None:
-            return {'error': 'not found'}, 404
-        return f'{idx} is deleted', 200
+# adding the defined resources along with their corresponding urls
+api.add_resource(Hello, '/')
+api.add_resource(Square, '/square/<int:num>')
 
-
-api.add_resource(GetData, '/')
-api.add_resource(AddData, '/add')
-api.add_resource(UpdateData, '/update/<int:id>')
-api.add_resource(DeleteData, '/delete/<int:id>')
-
-#
+# driver function
 if __name__ == '__main__':
     app.run(debug=True)
