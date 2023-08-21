a27800051b8ada87f2ee39f3c796239733c4c410
palindungan <44224923+palindungan@users.noreply.github.com>
2023-08-21 21:24:14 +0700

    Update app.py

--- API/Testing/app.py
+++ API/Testing/app.py
@@ -1,8 +1,11 @@
-from distutils.log import debug
-from fileinput import filename
+import numpy as np
+import cv2
+
 from flask import *
+from Util import MediapipeFaceMeshModule
 
 app = Flask(__name__)
+mediapipeFaceMesh = MediapipeFaceMeshModule.MediapipeFaceMesh()
 
 
 @app.route('/')
@@ -10,13 +13,25 @@ def main():
     return "index.html"
 
 
-@app.route('/success', methods=['POST'])
-def success():
+@app.route('/prediction', methods=['POST'])
+def prediction():
     if request.method == 'POST':
-        f = request.files['file']
-        # f.save(f.filename)
+        file = request.files['file']
+
+        # Convert the image data into a NumPy array
+        image_data = np.frombuffer(file.read(), np.uint8)
+
+        # Decode the image using OpenCV
+        img = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
+
         data = request.form
-        return {'data' : f.filename}, 201
+
+        # MediapipeFaceMesh processing
+        multi_face_landmarks = mediapipeFaceMesh.processing(img)
+        get_roi_images = mediapipeFaceMesh.get_roi_images(img, multi_face_landmarks)
+        roi_images, roi_bboxes = get_roi_images
+
+        return {'data': data}, 201
 
 
 if __name__ == '__main__':
